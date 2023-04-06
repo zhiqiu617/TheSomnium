1ffd321f9453eb799d4ae799a7248d09989bd34a
EG <eg104134@163.com>
2023-04-06 22:10:52 +0800

    init project

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'components': '@/components',
+        'views': '@/views'
+      }
+    }
+  }
+}
